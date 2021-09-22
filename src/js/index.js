db2af5c3e3b5d3e45b1e14a04c1f851335415df0
OLEG <schastliviy.sluchay@ukr.net>
2021-09-22 18:15:31 +0300

    Program 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,10 @@ import closeMobileMenu from './mobile-menu_closed';
 const menuButton = document.querySelector('.header__menu_button');
 const menu = document.querySelector('.menu');
 const closeButton = document.querySelector('.menu__close_button');
-const link = document.querySelector('.menu__link');
+const linkAbout = document.querySelector('#about_Link');
+const linkProgram = document.querySelector('#program_Link');
+
 
 openMobileMenu(menu, menuButton);
-closeMobileMenu(menu, menuButton, closeButton, link);
+closeMobileMenu(menu, menuButton, closeButton, linkAbout);
+closeMobileMenu(menu, menuButton, closeButton, linkProgram);
--- src/js/mobile-menu_closed.js
+++ src/js/mobile-menu_closed.js
@@ -7,6 +7,7 @@ export default function closeMobileMenu(menu, openButton, closeButton, link) {
     link.addEventListener('click', () => {
         menu.classList.add('menu_is-hidden');
         openButton.classList.remove('menu__button_is-hidden');
+        
     })
 
 }
